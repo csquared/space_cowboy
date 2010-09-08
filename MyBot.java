5a41b303146c2ea6a137ee2ee3ff9044978071d0
koryk <kory.kirk@gmail.com>
2010-09-07 21:34:55 -0400

    added some comments

--- MyBot.java
+++ MyBot.java
@@ -1,17 +1,15 @@
 import java.util.*;
 
 public class MyBot {
-    // The DoTurn function is where your code goes. The PlanetWars object
-    // contains the state of the game, including information about all planets
-    // and fleets that currently exist. Inside this function, you issue orders
-    // using the pw.IssueOrder() function. For example, to send 10 ships from
-    // planet 3 to planet 8, you would say pw.IssueOrder(3, 8, 10).
-    //
-    // There is already a basic strategy in place here. You can use it as a
-    // starting point, or you can throw it out entirely and replace it with
-    // your own. Check out the tutorials and articles on the contest website at
-    // http://www.ai-contest.com/resources.
-    public static void DoTurn(PlanetWars pw) {
+	/*
+	 * Build State of the world
+	 * take random guesses of next move
+	 * evaluate guesses choose best overall
+	 * opponent modeling
+	 * score = (total growth)*(total planets) + ships/(planets+growth) 
+	 */
+	
+	public static void DoTurn(PlanetWars pw) {
       // (1) If we currently have a fleet in flight, just do nothing.
       if (pw.MyFleets().size() >= 1) {
           return;
